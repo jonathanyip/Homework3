9d06edfce24e9b73d6c69bb2dd9264e12ca95e82
Jonathan Yip <jonathangyip@gmail.com>
2017-11-09 22:41:39 -0800

    Added mocking tests to test CourseManager

--- src/main/test/core/test/TestCourseManager.java
+++ src/main/test/core/test/TestCourseManager.java
@@ -1,50 +1,116 @@
 package core.test;
 
-import org.junit.Before;
-import org.junit.Test;
 import static org.junit.Assert.*;
 
-import org.mockito.Mock;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.AdditionalMatchers;
 import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
+import org.mockito.Spy;
 
-import core.api.CourseManager;
+import core.api.ICourseManager;
 import core.api.impl.Admin;
+import core.api.impl.CourseManager;
 
+/**
+ * Tests course manager. Since the Admin implementation is known to be bugging. 
+ * 
+ * @author Vincent
+ *
+ */
 public class TestCourseManager {
 
-	@Mock
+	@Spy
 	private Admin admin;
-	private CourseManager courseManager;
+	private ICourseManager courseManager;
 	
 	@Before
 	public void setup() {
-		this.admin = Mockito.mock(Admin.class);
+		this.admin = Mockito.spy(new Admin());
 		this.courseManager = new CourseManager(this.admin);
-
-		Mockito.doReturn(false).when(this.admin).classExists(Mockito.anyString(), Mockito.eq(2016));
-		Mockito.doCallRealMethod().when(this.admin).changeCapacity(Mockito.anyString(), Mockito.anyInt(), Mockito.anyInt());
-		
-		mockAdvanced();
+		setupMocking();
 	}
 
-	public void mockAdvanced() {
-		Mockito.doAnswer(new Answer<Void>() {
-			@Override
-			public Void answer(InvocationOnMock invocation) throws Throwable {
-				Object[] arguments = invocation.getArguments();
-				if ((int) arguments[1] >= 2017 && (int) arguments[3] > 0) {
-					admin.createClass((String) arguments[0], (int) arguments[1], (String) arguments[2], (int) arguments[3]);
-				}
-				return null;
-			}
-		}).when(this.admin).createClass(Mockito.anyString(), Mockito.anyInt(), Mockito.anyString(), Mockito.anyInt());
+	/*
+	 * Shows some initial set-up for the mocking of Admin.
+	 * This includes fixing a known bug (year in past is not correctly checked) in the Admin class by Mocking its behavior.
+	 * Not all fixes to Admin can be made from here, so for the more complex constraints you can simply Mock the
+	 * specific calls to Admin's createClass to yield the correct behavior in the unit test itself.
+	 */
+	public void setupMocking() {
+		Mockito.doNothing().when(this.admin).createClass(Mockito.anyString(), AdditionalMatchers.lt(2017), Mockito.anyString(), Mockito.anyInt());
+		Mockito.doNothing().when(this.admin).createClass(Mockito.anyString(), Mockito.anyInt(), Mockito.anyString(), AdditionalMatchers.leq(0));
+	}
+
+	@Test
+	public void testCreateClassCorrect() {
+		this.courseManager.createClass("ECS161", 2017, "Instructor", 1);
+		assertTrue(this.courseManager.classExists("ECS161", 2017));
 	}
 	
 	@Test
-	public void testCourseManagerCreateClass() {
-		this.courseManager.createClass("ECS161", 2016);
+	public void testCreateClassInPast() {
+		this.courseManager.createClass("ECS161", 2016, "Instructor", 1);
 		assertFalse(this.courseManager.classExists("ECS161", 2016));
+	}
+
+	@Test
+	public void testCreateClassInFuture() {
+		this.courseManager.createClass("ECS161", 2018, "Instructor", 1);
+		Mockito.verify(this.admin, Mockito.never()).createClass(Mockito.anyString(), Mockito.anyInt(), Mockito.anyString(), Mockito.anyInt());
+	}
+	
+	@Test
+	public void testCreateClass_capacityCorrect() {
+		this.courseManager.createClass("ECS161", 2017, "Instructor", 1);
+		assertTrue(this.courseManager.classExists("ECS161", 2017));
+	}
+	
+	@Test
+	public void testCreateClass_capacityZero() {
+		this.courseManager.createClass("ECS161", 2017, "Instructor", 0);
+		assertFalse(this.courseManager.classExists("ECS161", 2017));
+	}
+	
+	@Test
+	public void testCreateClass_capacityNegative() {
+		this.courseManager.createClass("ECS161", 2017, "Instructor", -1);
+		assertFalse(this.courseManager.classExists("ECS161", 2017));
+	}
+	
+	@Test
+	public void testCreateClass_capacityTooLarge() {
+		this.courseManager.createClass("ECS161", 2017, "Instructor", 1000);
+		Mockito.verify(this.admin, Mockito.never()).createClass(Mockito.anyString(), Mockito.anyInt(), Mockito.anyString(), Mockito.anyInt());
+	}
+	
+	@Test
+	public void testCreateClass_capacityNearTooLarge() {
+		this.courseManager.createClass("ECS161", 2017, "Instructor", 999);
+		assertTrue(this.courseManager.classExists("ECS161", 2017));
+	}
+	
+	@Test
+	public void testCreateClass_noMoreTwoClassPerInstructorCorrect() {
+		Mockito.doNothing().when(this.admin).createClass("ECS189", 2017, "Instructor", 1);
+		
+		this.courseManager.createClass("ECS161", 2017, "Instructor", 1);
+		this.courseManager.createClass("ECS160", 2017, "Instructor", 1);
+		this.courseManager.createClass("ECS189", 2017, "Instructor", 1);
+		
+		assertTrue(this.courseManager.classExists("ECS161", 2017));
+		assertTrue(this.courseManager.classExists("ECS160", 2017));
+		assertFalse(this.courseManager.classExists("ECS189", 2017));
+	}
+	
+	@Test
+	public void testCreateClass_noTwoClassesWithSameNameYear() {
+		Mockito.doNothing().when(this.admin).createClass("ECS161", 2017, "Instructor2", 1);
+		
+		this.courseManager.createClass("ECS161", 2017, "Instructor1", 1);
+		this.courseManager.createClass("ECS161", 2017, "Instructor2", 1);
+		
+		assertTrue(this.courseManager.classExists("ECS161", 2017));
+		assertEquals("Instructor1", this.courseManager.getClassInstructor("ECS161", 2017));
 	}
 }
